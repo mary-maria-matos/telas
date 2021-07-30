8c8fdd9edbcc6e254343d65d47475f1447870e94
mary-maria-matos <88064505+mary-maria-matos@users.noreply.github.com>
2021-07-30 20:45:59 -0300

    Add files via upload

---
+++ logar 1.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'conexao.php';
+
+    $email = $_GET['email'];
+    $senha = $_GET['senha'];
+
+    //executar uma pesquisa no bd
+    $sql = $dbcon=>query("SELECT * FROM tblogin WHERE email='$email' AND senha='$senha'");
+
+   /*  var_dump($sql); */
+
+     if(mysqli_num_rows($sql)>0){
+        echo "login_ok";
+    }else{
+        echo "login_erro";
+    } 
+
+?>
